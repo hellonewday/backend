87925b2ef558fce43cc844cbca3aca8b7c7113ca
Maximuss Hùng <vidostartup@gmail.com>
2019-07-04 20:15:41 +0700

    update bcrypt and PATCH avatarUrl with multer

--- routes/club.js
+++ routes/club.js
@@ -7,6 +7,33 @@ const Club = require("../models/Club");
 const Chatroom = require("../models/Chatroom");
 const router = require("express").Router();
 
+moment.locale("vi");
+cloudinary.config({
+  cloud_name: "vn-esports",
+  api_key: "996178356223912",
+  api_secret: "rC8_6QyIf1DIbokVgSYe0VLsJwQ"
+});
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  }
+});
+const filterUpload = function(req, file, cb) {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  fileFilter: filterUpload,
+  limits: 1024 * 1024 * 25
+});
+
 router.get("/", (req, res) => {
   Club.find()
     .sort({ quantity: 1 })
@@ -22,10 +49,32 @@ router.get("/", (req, res) => {
     .exec()
     .then(doc => {
       res.status(200).json({
-        data: doc
+        data: doc.map(item => {
+          return {
+            name: item.clubname,
+            club_id: item._id,
+            description: item.description,
+            level: item.level,
+            avatar: item.avatarUrl,
+            members: item.members.map(member => {
+              return {
+                member_name: member.nickname,
+                member_id: member._id
+              };
+            }),
+            quantity: item.quantity,
+            created_date: moment(item.created).format("LL"),
+            game: item.genre,
+            chatroom: {
+              name: item.chatroom.title,
+              chatroom_id: item.chatroom._id,
+              host: item.chatroom.host.nickname
+            }
+          };
+        })
       });
     })
-    .catch();
+    .catch(error => res.status(400).json({ error }));
 });
 router.post("/", isProtected, (req, res) => {
   let data = new Club({
@@ -73,6 +122,7 @@ router.get("/:clubId", (req, res) => {
       });
     });
 });
+
 router.patch("/:clubId", isProtected, async (req, res) => {
   let existClub = await Club.findOne({ _id: req.params.clubId });
   if (existClub && existClub.members[0] === req.userData._id) {
@@ -90,6 +140,40 @@ router.patch("/:clubId", isProtected, async (req, res) => {
       });
   }
 });
+
+router.patch("/:clubId/avatar", upload.single("avatar"), (req, res) => {
+  cloudinary.uploader
+    .upload(req.file.path, { resource_type: "image" })
+    .then(async doc => {
+      Club.updateOne({ _id: req.params.clubId }, { avatarUrl: doc.secure_url })
+        .exec()
+        .then(response => {
+          res.status(200).json({
+            message: "Update avatar successfully",
+            avatarUrl: doc.secure_url,
+            data: response
+          });
+        })
+        .catch(err => {
+          res.status(404).json({ error: err });
+        });
+    })
+    .catch(err => {
+      res
+        .status(400)
+        .json({ message: "Cannot upload image to server", error: err });
+    });
+});
+
+router.delete("/clubId", isProtected, (req, res) => {
+  Club.deleteOne({ _id: req.params.clubId })
+    .exec()
+    .then(response => {
+      res.status(200).json({ response });
+    })
+    .catch(error => res.status(400).json({ error }));
+});
+
 router.post("/:clubId/chat", isProtected, (req, res) => {
   let data = new Chatroom({
     title: req.body.title,
